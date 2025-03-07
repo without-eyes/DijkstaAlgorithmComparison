5f858e35183c3513d4803f14d86829b85483d6be
without eyes <37347347+without-eyes@users.noreply.github.com>
2025-03-07 13:03:08 +0200

    Fix console messages

--- src/Main.java
+++ src/Main.java
@@ -19,21 +19,21 @@ public class Main {
 
         // Run Dijkstra's algorithm with multithreading all vertices
         startTime = System.nanoTime();
-        System.out.println("\nDijkstra's algorithm with multithreading: ");
+        System.out.println("\nDijkstra's algorithm with multithreading all vertices: ");
         dijkstraAlgorithm.runAllVerticesMultithreaded(0, destination);
         long allVerticesMultithreadedEstimatedTime = System.nanoTime() - startTime;
         System.out.println("Estimated time: " + allVerticesMultithreadedEstimatedTime);
 
         // Run Dijkstra's algorithm with multithreading source's neighbours only
         startTime = System.nanoTime();
-        System.out.println("\nDijkstra's algorithm with multithreading: ");
-        dijkstraAlgorithm.runAllVerticesMultithreaded(0, destination);
+        System.out.println("\nDijkstra's algorithm with multithreading source's neighbours only: ");
+        dijkstraAlgorithm.runFirstNeighboursMultithreaded(0, destination);
         long firstNeighboursMultithreadedEstimatedTime = System.nanoTime() - startTime;
         System.out.println("Estimated time: " + firstNeighboursMultithreadedEstimatedTime);
 
         // Print results
-        System.out.println("\nRegular estimated time:                                    " + regularEstimatedTime);
-        System.out.println("Multithreading all vertices estimated time:                " + allVerticesMultithreadedEstimatedTime);
-        System.out.println("Multithreading source's neighbours only estimated time:    " + firstNeighboursMultithreadedEstimatedTime);
+        System.out.println("\nRegular estimated time:                                 " + regularEstimatedTime);
+        System.out.println("Multithreading all vertices estimated time:             " + allVerticesMultithreadedEstimatedTime);
+        System.out.println("Multithreading source's neighbours only estimated time: " + firstNeighboursMultithreadedEstimatedTime);
     }
 }
